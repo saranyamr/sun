e68c98b0771722d33dede6fde95d19a1b8c8e4f2
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:59:36 +0530

    comparison

---
+++ comparison.java
@@ -0,0 +1,12 @@
+public class comparison
+{
+    public static void main(String[] args)
+    {
+        String a="hello";
+        String b="world";
+        System.out.println("Result:"+(a==b));
+         int c=10;
+         int d=34;
+         System.out.println("Result:"+(c>d));
+    }
+}
