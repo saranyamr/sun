39724df1e65193598deaf7db935358ed7995a68c
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:28:01 +0530

    a

---
+++ week.java
@@ -0,0 +1,44 @@
+public class week{
+    public static void main(String[] args){
+        int day=4;
+        switch(day){
+            case 1:
+                {
+                    System.out.println("Sunday");
+                }
+                break;
+                case 2:
+                    {
+                        System.out.println("Monday");
+                    }
+                    break;
+                    case 3:
+                        {
+                            System.out.println("Tuesday");
+                        }
+                        break;
+                        case 4:
+                            {
+                                System.out.println("Wednesday");
+                            }
+                            break;
+                            case 5:
+                                {
+                                    System.out.println("Thursday");
+                                }
+                                break;
+                                case 6:
+                                    {
+                                        System.out.println("Friday");
+                                    }
+                                    break;
+                                    default:
+                                        {
+                                            System.out.println("it is not a day");
+
+                                        }
+                                        break;
+        }
+    }
+}
+
