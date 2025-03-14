38cf0164f03e31e6175bd49ea0db4f85b5fd3c5d
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:48:36 +0530

    d

---
+++ ifstat.java
@@ -0,0 +1,21 @@
+public class ifstat
+{
+    public static void main(String[] args)
+    {
+        int age=23;
+        if (age>=18)
+        {
+            System.out.println("major");
+        }
+        else{
+            System.out.println("minor");
+        }
+        }
+    }
+
+            
+
+        
+    
+    
+    
