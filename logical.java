9e66bb5fe51bc083ee1848951f19e42e4fe23fa8
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:50:17 +0530

    k

---
+++ logical.java
@@ -0,0 +1,12 @@
+public class logical
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=17;
+        boolean c=a>b;
+        boolean d=a==b;
+        System.out.println((a>b)&&(a==b));
+        System.out.println((a<b)||(a==b));
+    }
+}
