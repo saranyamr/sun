f2727064ce0ac7df4d24e73bc2ed495b86150cda
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:45:30 +0530

    e

---
+++ breg.java
@@ -0,0 +1,11 @@
+public class breg{
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println("hello");
+            if(i==5)
+            {
+                break;
+            }
+        }
+    }
+}
