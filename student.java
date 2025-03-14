b17f7bf5e820e4448223a20e8b90ce3bc3d6f602
saranyamr <saranyaprem246@gmail.com>
2025-03-14 10:14:28 +0530

    hi

---
+++ student.java
@@ -0,0 +1,14 @@
+public class student
+{
+    public static void main(String[] args)
+    {
+    String name="Saranya";
+    String subject="Science";
+    int mark=91;
+    char grade='a';
+    System.out.println("Name is:"+Name);
+    System.out.println("Subname is:"+Subject);
+    System.out.println("Mark is:"+Mark);
+    System.out.println("Grade is:"+Grade);
+    }
+}
