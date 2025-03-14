8ec34acdc1b5ff2d0a6b91621e06c4b900a3617b
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:49:27 +0530

    k

---
+++ concat.java
@@ -0,0 +1,12 @@
+public class concat
+{
+    public static void main(String[] args)
+    {
+        String a="hello";
+        String b="world";
+        String c=a+b;
+        System.out.println(c);
+    } 
+}          
+    
+
