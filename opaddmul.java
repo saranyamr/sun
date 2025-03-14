23e5e3e31f73fbda9ec05eb91ae3b32c5fbeedef
saranyamr <saranyaprem246@gmail.com>
2025-03-14 11:33:28 +0530

    a

---
+++ opaddmul.java
@@ -0,0 +1,12 @@
+public class  opaddmul
+{
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=23;
+        int c=5;
+        int d=4;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Multiplication:"+(c*d));
+    }
+}
