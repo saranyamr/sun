b2ff509379cd20783157555c65ca807a54a24d7c
saranyamr <saranyaprem246@gmail.com>
2025-03-14 13:17:50 +0530

    s

---
+++ mul.java
@@ -0,0 +1,13 @@
+public class mul{
+    static void mulmethod(){
+        int a=5;
+        int b=8;
+        System.out.println("multiplication:"+a*b);
+    
+    }
+    public static void main(String[] args)
+    {
+        mulmethod();
+
+    }
+}
