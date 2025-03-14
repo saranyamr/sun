685eabb506447d84f7f6148cdd5fbdb6bdf876ee
saranyamr <saranyaprem246@gmail.com>
2025-03-14 10:42:33 +0530

    hi

---
+++ loop2.java
@@ -0,0 +1,10 @@
+public class loop2{
+    public static void mains(String[] args){
+        int i=1;
+        while(i<=5)
+        {
+            System.out.println(10);
+            i++;
+        }
+    }
+}
