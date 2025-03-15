c40ad9b8f0a3c0fdb9336126ae3ecfcb5a7b0014
saranyamr <saranyaprem246@gmail.com>
2025-03-15 11:57:52 +0530

    scanner

---
+++ scanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class scanner
+{
+    public static void main(String[] args)
+    {
+        Scanner a=new Scanner(System.in);
+        System.out.println("Enter username:");
+        String username=a.nextline();
+        System.out.println("The username is:"+username);
+    }
+}
