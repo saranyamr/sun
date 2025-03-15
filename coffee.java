61e961d4d946d0eebf99ec59c76b54553cd367cd
saranyamr <saranyaprem246@gmail.com>
2025-03-15 11:14:54 +0530

    coffee

---
+++ coffee.java
@@ -0,0 +1,26 @@
+public class coffee{
+    public static void main(String[] args){
+        int choice=3;
+        switch(choice)
+        {
+            case 1:{
+                System.out.println("hot coffee");
+            }
+            break;
+            case 2:{
+                System.out.println("cold coffee");
+            }
+            break;
+            case 3:{
+                System.out.println("badam coffee");
+            }
+            break;
+            case 4:{
+                System.out.println("choco coffee");
+            }break;
+            default:{
+                System.out.println("no coffee");}
+            break;
+        }
+    }
+}
