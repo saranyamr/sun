101c3b0ab8fe0b194f5c9947d682a047764336f2
saranyamr <saranyaprem246@gmail.com>
2025-03-15 10:37:14 +0530

    vowel'

---
+++ vowel.java
@@ -0,0 +1,41 @@
+public class vowel
+{
+public static void main(String[] args)
+{
+char choice='a';
+switch(choice)
+{
+    case 'a':
+        {
+        System.out.println("vowel");
+    }
+    break;
+    case 'e':
+        {
+        System.out.println("vowel");
+        }
+        break;
+        case 'i':
+            {
+            System.out.println("vowel");
+            }
+            break;
+            case 'o':
+                {
+                System.out.println("vowel");
+            }
+            break;
+            case 'u':
+                {
+
+                System.out.println("vowel");
+            }
+            break;
+            default:
+            {
+            System.out.println("not a vowel:");
+            }
+            break;
+}
+}
+}
