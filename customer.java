9e85a64bd66720be0f6f0f9cf9cd2d3e32c99374
saranyamr <saranyaprem246@gmail.com>
2025-03-15 10:47:25 +0530

    customer

---
+++ customer.java
@@ -0,0 +1,11 @@
+public class customer
+{
+    public static void main()
+    {
+        int [] a={30,50,60,70,90};
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
