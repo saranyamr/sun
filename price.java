816bd6b3d595876ccae48afd834cc8ea6a0a8a4a
saranyamr <saranyaprem246@gmail.com>
2025-03-15 11:06:05 +0530

    price

---
+++ price.java
@@ -0,0 +1,9 @@
+public class price{
+    public static void main(String[] args){
+        int [] cost={10,2,26,28};
+        System.out.println("The price of pen is:+cost[0]");
+        System.out.println("The price of pencil:+cost[1]");
+        System.out.println("The price of bat:+cost[2]");
+        System.out.println("The price of ball:+cost[3]");
+    }
+}
